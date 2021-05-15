eff14774c1dcc900edaf79494098a6723e822f75
Vanessa Swerts <vanessaswerts@gec.inatel.br>
2021-05-15 10:21:17 -0300

    created user table

---
+++ src/services/UsersService.ts
@@ -0,0 +1,30 @@
+import { getCustomRepository } from "typeorm"
+import { UsersRepository } from "../repositories/UsersRepository"
+
+class UsersService {
+  async create(email: string) {
+    const usersRepository = getCustomRepository(UsersRepository)
+
+    // Verificar se  o usuário existe
+    const userExists = await usersRepository.findOne({
+      email
+    })
+
+    // Se existir, retornar user
+    if (userExists) {
+      return userExists
+    }
+
+    // Se não existir, salvar no BD
+    const user = usersRepository.create({
+      email
+    })
+
+    await usersRepository.save(user)
+
+    return user
+
+  }
+}
+
+export { UsersService }
