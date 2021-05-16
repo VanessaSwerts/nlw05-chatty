9b70469498c6c89cffdaaadc3494f19021f76c8f
Vanessa Swerts <vanessaswerts@gec.inatel.br>
2021-05-16 13:55:58 -0300

    create message table

--- routes.ts
+++ routes.ts
@@ -1,13 +1,19 @@
 import { Router } from 'express'
 import { SettingsController } from './src/controllers/SettingsController'
 import { UsersController } from './src/controllers/UsersController'
+import { MessagesController } from './src/controllers/MessagesController'
 
 const routes = Router()
 
 const settingsController = new SettingsController()
 const usersController = new UsersController()
+const messageController = new MessagesController()
 
 routes.post("/settings", settingsController.create)
+
 routes.post("/users", usersController.create)
+
+routes.post("/messages", messageController.create)
+routes.get("/messages/:id", messageController.showByUser)
 
 export { routes }
